27bd274165cf9dd8c11e7af557810e4fb0530109
Harrison Boyd <harrisonboyd630@gmail.com>
2017-04-19 11:04:36 -0400

    added shirts

--- js/museumjs/general.js
+++ js/museumjs/general.js
@@ -6,7 +6,7 @@
 // document.getElementById('up').onmouseout = function(){
 //    document.getElementById('sizes').style.display = 'none';
 // }
-
+//FIRST ANIMATE
 document.getElementById("tshirts").style.display = "none";
 
 document.getElementById("dropUp").onmouseover = function(){
@@ -23,4 +23,40 @@ document.getElementById("color-box").onmouseover = function() {
 }
 document.getElementById("color-box").onmouseout = function() {
 	document.getElementById("colors").style.display = "none";
+}
+//SECOND ANIMATE
+document.getElementById("tshirts2").style.display = "none";
+
+document.getElementById("dropUp2").onmouseover = function(){
+	document.getElementById("tshirts2").style.display = "block";
+}
+document.getElementById("dropUp2").onmouseout = function() {
+	document.getElementById("tshirts2").style.display = "none";
+}
+
+document.getElementById("colors2").style.display = "none";
+
+document.getElementById("color-box2").onmouseover = function() {
+	document.getElementById("colors2").style.display = "block";
+}
+document.getElementById("color-box2").onmouseout = function() {
+	document.getElementById("colors2").style.display = "none";
+}
+//THIRD ANIMATE
+document.getElementById("tshirts3").style.display = "none";
+
+document.getElementById("dropUp3").onmouseover = function(){
+	document.getElementById("tshirts3").style.display = "block";
+}
+document.getElementById("dropUp3").onmouseout = function() {
+	document.getElementById("tshirts3").style.display = "none";
+}
+
+document.getElementById("colors3").style.display = "none";
+
+document.getElementById("color-box3").onmouseover = function() {
+	document.getElementById("colors3").style.display = "block";
+}
+document.getElementById("color-box3").onmouseout = function() {
+	document.getElementById("colors3").style.display = "none";
 }
